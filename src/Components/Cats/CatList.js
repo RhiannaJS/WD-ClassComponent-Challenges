1813407dc85340d430e67fcfff54c2ba43ccf11b
RhiannaJS <rhianna.latshaw@gmail.com>
2021-09-20 08:35:28 -0400

    cats & dogs

--- src/Components/Cats/CatList.js
+++ src/Components/Cats/CatList.js
@@ -3,17 +3,14 @@ import React, { Component, List } from 'react';
  
 const CatList = (props) => {
   
-  // return( props.cat.map((cat, index) =>{
+  
       
   
  return (
    <div>
      {props.cats.map(cat=><li>{cat}</li>)}
      
-     {/* <ul>
-      <li key={index}>{props.cat}</li> 
-    </ul>  
-    */}
+     
    </div>
  )
  }
